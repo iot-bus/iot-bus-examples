860d8b0734218ce229b9457260f9c00efe96f7a7
ianarchbell <ian@Ians-iMac-9.local>
2018-11-06 12:24:31 -0800

    Run through compilation tests

--- iot-bus-canbus/iot-bus-canbus-receive/src/main.cpp
+++ iot-bus-canbus/iot-bus-canbus-receive/src/main.cpp
@@ -1,41 +1,55 @@
-#include <ESP32CAN.h>
-#include <CAN_config.h>
+// Copyright (c) Sandeep Mistry. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-CAN_device_t CAN_cfg;
+#include <CAN.h>
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println("iotsharing.com CAN demo - receive");
-    CAN_cfg.speed=CAN_SPEED_1000KBPS;
-    CAN_cfg.tx_pin_id = GPIO_NUM_5;
-    CAN_cfg.rx_pin_id = GPIO_NUM_4;
-    CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
-    //start CAN Module
-    ESP32Can.CANInit();
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("CAN Receiver");
+
+  // start the CAN bus at 1000 kbps
+  if (!CAN.begin(1000E3)) {
+    Serial.println("Starting CAN failed!");
+    while (1);
+  }
 }
 
 void loop() {
-    CAN_frame_t rx_frame;
-    //receive next CAN frame from queue
-    if(xQueueReceive(CAN_cfg.rx_queue,&rx_frame, 3*portTICK_PERIOD_MS)==pdTRUE){
+  // try to parse packet
+  int packetSize = CAN.parsePacket();
 
-      //do stuff!
-      if(rx_frame.FIR.B.FF==CAN_frame_std){
-        printf("New standard frame");
-        digitalWrite(12, HIGH);
-      }
-      else{
-        printf("New extended frame");
-      }
-      if(rx_frame.FIR.B.RTR==CAN_RTR){
-        printf(" RTR from 0x%08x, DLC %d\r\n",rx_frame.MsgID,  rx_frame.FIR.B.DLC);
-      }
-      else{
-        printf(" from 0x%08x, DLC %d\n",rx_frame.MsgID,  rx_frame.FIR.B.DLC);
-        for(int i = 0; i < 8; i++){
-          printf("%c\t", (char)rx_frame.data.u8[i]);
-        }
-        printf("\n");
-      }
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received ");
+
+    if (CAN.packetExtended()) {
+      Serial.print("extended ");
     }
+
+    if (CAN.packetRtr()) {
+      // Remote transmission request, packet contains no data
+      Serial.print("RTR ");
+    }
+
+    Serial.print("packet with id 0x");
+    Serial.print(CAN.packetId(), HEX);
+
+    if (CAN.packetRtr()) {
+      Serial.print(" and requested length ");
+      Serial.println(CAN.packetDlc());
+    } else {
+      Serial.print(" and length ");
+      Serial.println(packetSize);
+
+      // only print packet data for non-RTR packets
+      while (CAN.available()) {
+        Serial.print((char)CAN.read());
+      }
+      Serial.println();
+    }
+
+    Serial.println();
+  }
 }
--- iot-bus-canbus/iot-bus-canbus-send-hello/src/main.cpp
+++ iot-bus-canbus/iot-bus-canbus-send-hello/src/main.cpp
@@ -1,32 +1,43 @@
-#include <ESP32CAN.h>
-#include <CAN_config.h>
-
-CAN_device_t CAN_cfg;
+#include <CAN.h>
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("iotsharing.com CAN demo - send hello");
-    CAN_cfg.speed=CAN_SPEED_1000KBPS;
-    CAN_cfg.tx_pin_id = GPIO_NUM_5;
-    CAN_cfg.rx_pin_id = GPIO_NUM_4;
-    //start CAN Module
-    ESP32Can.CANInit();
+ 
+     // start the CAN bus at 1000 kbps
+    if (!CAN.begin(1000E3)) {
+        Serial.println("Starting CAN failed!");
+        while (1);
+    }
 }
 
 void loop() {
-      CAN_frame_t rx_frame;
-      
-      rx_frame.FIR.B.FF = CAN_frame_std;
-      rx_frame.MsgID = 1;
-      rx_frame.FIR.B.DLC = 8;
-      rx_frame.data.u8[0] = 'h';
-      rx_frame.data.u8[1] = 'e';
-      rx_frame.data.u8[2] = 'l';
-      rx_frame.data.u8[3] = 'l';
-      rx_frame.data.u8[4] = 'o';
-      rx_frame.data.u8[5] = 'c';
-      rx_frame.data.u8[6] = 'a';
-      rx_frame.data.u8[7] = 'n';
-      
-      ESP32Can.CANWriteFrame(&rx_frame);
+    // send packet: id is 11 bits, packet can contain up to 8 bytes of data
+    Serial.print("Sending packet ... ");
+
+    CAN.beginPacket(0x12);
+    CAN.write('h');
+    CAN.write('e');
+    CAN.write('l');
+    CAN.write('l');
+    CAN.write('o');
+    CAN.endPacket();
+
+    Serial.println("done");
+
+    delay(1000);
+
+    // send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
+    Serial.print("Sending extended packet ... ");
+
+    CAN.beginExtendedPacket(0xabcdef);
+    CAN.write('w');
+    CAN.write('o');
+    CAN.write('r');
+    CAN.write('l');
+    CAN.write('d');
+    CAN.endPacket();
+
+    Serial.println("done");
+
+    delay(1000);
 }
